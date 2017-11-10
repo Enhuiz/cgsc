ca46577dc50e304c58e9d1adb33ba04474c4e547
enhuiz <niuzhe.nz@outlook.com>
2017-11-10 12:17:36 +0800

    update font size

--- scripts/bnb_visual.py
+++ scripts/bnb_visual.py
@@ -2,6 +2,7 @@ import pandas as pd
 import json
 from utils.path import data_dir, fig_dir
 import matplotlib.pyplot as plt
+import matplotlib 
 
 vinfo_path = data_dir(['visualize', 'bnb', 'vinfo.json'])
 fig_path = fig_dir(['bnb_visual'])
@@ -9,6 +10,8 @@ fig_path = fig_dir(['bnb_visual'])
 df = pd.DataFrame(json.load(open(vinfo_path, 'r')))
 df = df.set_index('t')
 
+matplotlib.rcParams.update({'font.size': 18})
+
 for col in df.columns:
     df.plot(y=col, figsize=(15,10))
-    plt.savefig(fig_path + '/0.5%/{}.png'.format(col))
+    plt.savefig(fig_path + '/1%/{}.png'.format(col))
