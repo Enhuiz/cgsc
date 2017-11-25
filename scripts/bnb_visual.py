eb05b9eb5c4242b59df40cfc2ae93db8e35aeef2
enhuiz <niuzhe.nz@outlook.com>
2017-11-25 11:21:30 +0800

    adapt for both format

--- scripts/bnb_visual.py
+++ scripts/bnb_visual.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import json
 from utils.path import data_dir, fig_dir
@@ -19,7 +20,16 @@ for dirpath, dirnames, filenames in os.walk(vinfo_path):
         roi_ratio = s.strip().split('-')[2]
         if roi_ratio not in d:
             d[roi_ratio] = {}
-        for k, v in json.load(open(os.path.join(dirpath, filename), 'r'))[0].items():
+        reports = json.load(open(os.path.join(dirpath, filename), 'r'))
+        if len(reports) == 1: # old version
+            reports = reports[0].items()
+        else: # new version
+            _ = {}
+            for k in reports[0].keys():
+                _[k] = [report[k] for report in reports]
+            reports = _.items()
+        for k, v in reports:
+            k = k.replace('price', 'cost')
             if k not in d[roi_ratio]:
                 d[roi_ratio][k] = {}
             d[roi_ratio][k][btype] = v
@@ -29,12 +39,22 @@ for roi_ratio, subd in d.items():
     if not os.path.exists(dirpath):
         os.makedirs(dirpath)
     for k, subsubd in subd.items():
-        if k == 't': continue
-        filepath = os.path.join(dirpath, '{}.png'.format(k))
-        fig, ax = plt.subplots(figsize=(15, 10))
-        for btype, v in subsubd.items():
-            df = pd.DataFrame({'t': subd['t'][btype], btype + ' ' + k: subsubd[btype]})
-            df = df.set_index('t')
-            df = df.iloc[::max(1, len(df)//10000), :]
-            df.plot(figsize=(15,10), ax=ax)
-        plt.savefig(filepath)
+        if k != 't': 
+            filepath = os.path.join(dirpath, '{}.png'.format(k))
+            fig, ax = plt.subplots(figsize=(15, 10))
+            for btype, v in subsubd.items():
+                df = pd.DataFrame({'t': subd['t'][btype], btype + ' ' + k: subsubd[btype]})
+                df = df.set_index('t')
+                df = df.iloc[::max(1, len(df)//10000), :]
+                df.plot(figsize=(15,10), ax=ax)
+            plt.savefig(filepath)
+        else:
+            k = 'number of iteration'
+            filepath = os.path.join(dirpath, '{}.png'.format(k))
+            fig, ax = plt.subplots(figsize=(15, 10))
+            for btype, v in subsubd.items():
+                df = pd.DataFrame({'t': subd['t'][btype], btype + ' ' + k: range(len(subd['t'][btype]))})
+                df = df.set_index('t')
+                df = df.iloc[::max(1, len(df)//10000), :]
+                df.plot(figsize=(15,10), ax=ax)
+            plt.savefig(filepath)
