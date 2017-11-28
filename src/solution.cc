4504ea788d21301336a15508a347688ddb163ffe
enhuiz <niuzhe.nz@outlook.com>
2017-11-28 10:29:09 +0800

    code optimization

--- src/solution.cc
+++ src/solution.cc
@@ -23,7 +23,7 @@ double calculate_density(const Entity &roi, const list<Entity> &entities)
 
 Greedy::Greedy(const Range &const_universe, const list<Range> &const_ranges, double target_coverage)
 {
-    auto universe = Range(const_universe);
+    auto universe = const_universe;
     auto ranges = list<Range>(const_ranges.begin(), const_ranges.end());
     double value = 0;
     double target_value = universe.value * target_coverage;
@@ -86,43 +86,41 @@ struct Node
     double value = 0; // current covered area
     double cost_lower_bound;
 
-    using RangeConstIter = list<Range>::const_iterator;
-
-    list<RangeConstIter> selected;
-    list<RangeConstIter> to_select;
-    unordered_set<Element> elements;
+    list<int> selected;
+    int cursor = 0;
+    vector<bool> visited;
 
     static double target_value;
     static const Range *universe;
+    static vector<Range> ranges;
 
     list<shared_ptr<Node>> branch()
     {
         list<shared_ptr<Node>> ret;
 
-        { // discard
+        { // not select
             auto node = shared_ptr<Node>(new Node(*this));
-            node->to_select.pop_front();
+            ++node->cursor;
             ret.push_back(node);
         }
 
-        { // adopt
+        { // select
             auto node = shared_ptr<Node>(new Node(*this));
-            auto current = to_select.front();
-            node->to_select.pop_front();
-            // offcuts & covered
-            for (const auto &element : current->elements)
+            // offcuts & value
+            const auto &range = ranges[cursor];
+            for (const auto &element : range.elements)
             {
-                node->elements.insert(element);
+                if (node->visited[element.index] == 0)
+                {
+                    node->visited[element.index] = 1;
+                    node->value += element.value;
+                }
             }
             // selected ranges
-            node->selected.push_back(current);
+            node->selected.push_back(cursor);
             // cost
-            node->cost += current->cost;
-            // deficit area
-            node->value = func::sum(node->elements, [](const Element &e) {
-                return e.value;
-            });
-            // remove
+            node->cost += range.cost;
+            ++node->cursor;
             ret.push_back(node);
         }
         return ret;
@@ -132,26 +130,27 @@ struct Node
     {
         cost_lower_bound = cost;
         double current_value = value;
-        auto visited_elements = elements;
-        for (const auto &rit : to_select) // n
+        auto visited_copy = visited;
+        for (int i = cursor; i < ranges.size(); ++i)
         {
-            for (const auto &element : rit->elements) // k
+            const auto &range = ranges[i];
+            for (const auto &element : range.elements) // k
             {
-                if (visited_elements.count(element) == 0)
+                if (visited_copy[element.index] == 0)
                 {
                     double deficit = target_value - current_value;
                     if (element.value > deficit)
                     {
-                        cost_lower_bound += rit->cost / rit->value * deficit;
+                        cost_lower_bound += range.cost / range.value * deficit;
                         current_value += deficit;
                         return;
                     }
                     else
                     {
-                        cost_lower_bound += rit->cost / rit->value * element.value;
+                        cost_lower_bound += range.cost / range.value * element.value;
                         current_value += element.value;
                     }
-                    visited_elements.insert(element);
+                    visited_copy[element.index] = 1;
                 }
             }
         }
@@ -162,51 +161,64 @@ struct Node
         }
     }
 
+    nlohmann::json describe()
+    {
+        int n_visited = 0;
+        for (int i = 0; i < visited.size(); ++i)
+        {
+            n_visited += visited[i];
+        }
+        return nlohmann::json{
+            {"cost", cost},
+            {"to_select.size()", ranges.size() - cursor},
+            {"selected.size()", selected.size()},
+            {"visited.size()", n_visited},
+            {"coverage_ratio", value / universe->value},
+            {"cost lower bound", cost_lower_bound},
+        };
+    }
+
     void print(const string &s = "")
     {
         assert(universe != nullptr);
         logger << s << endl;
-        logger << "cost: " << cost << endl;
-        logger << "coverage ratio: " << value / universe->value << endl;
-        logger << "cost_lower_bound: " << cost_lower_bound << endl;
-        logger << "elements.size(): " << elements.size() << endl;
-        logger << "to_select.size(): " << to_select.size() << endl;
-        logger << "selected.size(): " << selected.size() << endl;
+        auto desc = describe();
+        for (auto it = desc.begin(); it != desc.end(); ++it)
+        {
+            logger << it.key() << ": " << it.value() << endl;
+        }
     }
 };
 
 double Node::target_value = 0;
 const Range *Node::universe = nullptr;
+vector<Range> Node::ranges;
 
 Bnb::Bnb(const Range &const_universe, const list<Range> &const_ranges, double target_coverage)
 {
     auto sw = Stopwatch(); // timer
-    auto universe = Range(const_universe);
-    auto ranges = list<Range>(const_ranges.begin(), const_ranges.end());
+    auto universe = const_universe;
+    Node::ranges = vector<Range>(const_ranges.begin(), const_ranges.end());
+    sort(Node::ranges.begin(), Node::ranges.end(), [](const Range &a, const Range &b) { // unit cost from lower to higher
+        return a.cost / a.value < b.cost / b.value;
+    });
     Node::target_value = universe.value * target_coverage;
     Node::universe = &universe;
     auto optimal_node = shared_ptr<Node>(new Node{});
     {
         auto greedy = Greedy(const_universe, const_ranges, target_coverage);
         optimal_node->cost = func::sum(greedy.subranges,
-                                       [](decltype(ranges)::const_iterator rit) {
+                                       [](list<Range>::const_iterator rit) {
                                            return rit->cost;
                                        });
-        // optimal_node->cost = 10000;
         optimal_node->print("initial optimal");
     }
     auto initial_node = shared_ptr<Node>(new Node{});
     {
         initial_node->cost = 0;
         initial_node->value = 0;
-        for (auto it = ranges.begin(); it != ranges.end(); ++it)
-        {
-            initial_node->to_select.push_back(it);
-        }
-        initial_node->to_select.sort([](list<Range>::const_iterator a,
-                                        list<Range>::const_iterator b) {
-            return a->cost / a->value < b->cost / b->value;
-        });
+        initial_node->cursor = 0;
+        initial_node->visited = vector<bool>(universe.elements.size());
         initial_node->bound();
         initial_node->print("initial node");
     }
@@ -221,17 +233,13 @@ Bnb::Bnb(const Range &const_universe, const list<Range> &const_ranges, double ta
         nodes.pop();
 
         sw.pause(); // pause the timer for debug informaiton
+        double t = sw.lap();
         logger << nodes.size() << endl;
-        debug_report.push_back({
-            {"cost", node->cost},
-            {"coverage_ratio", node->value / universe.value},
-            {"selected.size()", node->selected.size()},
-            {"to_select.size()", node->to_select.size()},
-            {"t", sw.lap()},
-            {"number of nodes", nodes.size()},
-            {"cost lower bound", node->cost_lower_bound},
-            {"cost upper bound", optimal_node->cost},
-        });
+        auto desc = node->describe();
+        desc["t"] = t; // to avoid lag
+        desc["number of nodes"] = nodes.size();
+        desc["cost upper bound"] = optimal_node->cost;
+        debug_report.push_back(desc);
         sw.continue_();
 
         if (node->value >= Node::target_value) // finished cover
@@ -243,7 +251,7 @@ Bnb::Bnb(const Range &const_universe, const list<Range> &const_ranges, double ta
             }
             // else keeping branching (add more ranges) will be no good, just kill the node.
         }
-        else if (node->to_select.size() > 0)
+        else if (node->cursor != Node::ranges.size())
         {
             auto new_nodes = node->branch();
             for (auto new_node : new_nodes)
@@ -260,72 +268,48 @@ Bnb::Bnb(const Range &const_universe, const list<Range> &const_ranges, double ta
 
 namespace old
 {
-void remove_ranges_disjoint_with(list<list<Range>::const_iterator> &ranges, const list<Polygon> &polys)
-{
-    ranges.erase(remove_if(ranges.begin(),
-                           ranges.end(),
-                           [&polys](list<Range>::const_iterator rit) {
-                               return none_of(polys.begin(), polys.end(), [rit](const Polygon &poly) {
-                                   return intersects(rit->entity->poly, poly);
-                               });
-                           }),
-                 ranges.end());
-}
 
 struct Node
 {
-    double cost;             // the cost of selected ranges
-    double value;            // value currently gained
-    double cost_lower_bound; // if the lower bound higher than the optimal, kill it
+    double cost = 0;  // the cost of selected ranges
+    double value = 0; // current covered area
+    double cost_lower_bound;
 
-    using RangeConstIter = list<Range>::const_iterator;
-
-    list<RangeConstIter> selected;
-    list<RangeConstIter> to_select;
-    list<Polygon> offcuts;
+    list<int> selected;
+    int cursor = 0; // next range to select (or not select)
 
     static double target_value;
     static const Range *universe;
+    static vector<Range> ranges;
+
+    list<Polygon> offcuts;
 
     list<shared_ptr<Node>> branch()
     {
         list<shared_ptr<Node>> ret;
 
-        { // discard
+        { // not select
             auto node = shared_ptr<Node>(new Node(*this));
-            node->to_select.pop_front();
+            ++node->cursor;
             ret.push_back(node);
         }
 
-        { // adopt
+        { // select
             auto node = shared_ptr<Node>(new Node(*this));
-            auto current_range = to_select.front();
-            node->to_select.pop_front();
+            const auto &range = ranges[cursor];
             // offcuts & covered
-            node->offcuts = difference(offcuts, {current_range->entity->poly});
+            node->offcuts = difference(offcuts, {range.entity->poly});
             // selected ranges
-            node->selected.push_back(current_range);
+            node->selected.push_back(cursor);
             // cost
-            node->cost += current_range->cost;
+            node->cost += range.cost;
             // deficit area
             node->value = universe->value - func::sum(node->offcuts, area);
             // remove
-            // remove_ranges_disjoint_with(node->to_select, node->offcuts);
+            ++node->cursor;
             ret.push_back(node);
         }
         return ret;
-    }
-
-    void print(const string &s = "")
-    {
-        assert(universe != nullptr);
-        logger << s << endl;
-        logger << "cost: " << cost << endl;
-        logger << "coverage ratio: " << value / universe->value << endl;
-        logger << "cost_lower_bound: " << cost_lower_bound << endl;
-        logger << "offcuts.size(): " << offcuts.size() << endl;
-        logger << "to_select.size(): " << to_select.size() << endl;
-        logger << "selected.size(): " << selected.size() << endl;
     }
 
     void bound()
@@ -333,11 +317,12 @@ struct Node
         cost_lower_bound = cost;
         auto left_offcuts = offcuts;
         double current_value = value;
-        for (auto rit : to_select)
+        for (int i = cursor; i < ranges.size(); ++i)
         {
             for (auto it = left_offcuts.begin(); it != left_offcuts.end();)
             {
-                auto scene_offcuts = intersection(universe->entity->poly, rit->entity->poly);
+                const auto &range = ranges[i];
+                auto scene_offcuts = intersection(universe->entity->poly, range.entity->poly);
                 if (scene_offcuts.size() == 0) // no intersection
                     continue;
                 auto scene_offcut = scene_offcuts.front();
@@ -353,13 +338,13 @@ struct Node
                         double inner_area = area(inner);
                         if (deficit <= inner_area)
                         {
-                            cost_lower_bound += rit->cost / rit->value * deficit;
+                            cost_lower_bound += range.cost / range.value * deficit;
                             current_value += deficit;
                             return; // value is enough, return
                         }
                         else
                         {
-                            cost_lower_bound += rit->cost / rit->value * inner_area;
+                            cost_lower_bound += range.cost / range.value * inner_area;
                             current_value += inner_area;
                         }
                     }
@@ -380,23 +365,50 @@ struct Node
             cost_lower_bound = numeric_limits<double>::max();
         }
     }
+
+    nlohmann::json describe()
+    {
+        return nlohmann::json{
+            {"cost", cost},
+            {"to_select.size()", ranges.size() - cursor},
+            {"selected.size()", selected.size()},
+            {"offcuts.size()", offcuts.size()},
+            {"coverage_ratio", value / universe->value},
+            {"cost lower bound", cost_lower_bound},
+        };
+    }
+
+    void print(const string &s = "")
+    {
+        assert(universe != nullptr);
+        logger << s << endl;
+        auto desc = describe();
+        for (auto it = desc.begin(); it != desc.end(); ++it)
+        {
+            logger << it.key() << ": " << it.value() << endl;
+        }
+    }
 };
 
 double Node::target_value = 0;
 const Range *Node::universe = nullptr;
+vector<Range> Node::ranges;
 
 Bnb::Bnb(const Range &const_universe, const list<Range> &const_ranges, double target_coverage)
 {
-    auto sw = Stopwatch();
+    auto sw = Stopwatch(); // timer
     auto universe = const_universe;
-    auto ranges = const_ranges;
+    Node::ranges = vector<Range>(const_ranges.begin(), const_ranges.end());
+    sort(Node::ranges.begin(), Node::ranges.end(), [](const Range &a, const Range &b) { // unit cost from lower to higher
+        return a.cost / a.value < b.cost / b.value;
+    });
     Node::target_value = area(universe.entity->poly) * target_coverage;
     Node::universe = &universe;
 
     auto optimal_node = shared_ptr<Node>(new Node{});
     {
-        optimal_node->cost = 24000.0;
-        // optimal_node->cost = 10000;
+        // optimal_node->cost = 24000.0;
+        optimal_node->cost = 10000;
         optimal_node->print("initial optimal");
     }
 
@@ -404,15 +416,9 @@ Bnb::Bnb(const Range &const_universe, const list<Range> &const_ranges, double ta
     {
         initial_node->cost = 0;
         initial_node->value = 0;
-        initial_node->offcuts = {universe.entity->poly};
-        for (auto it = ranges.begin(); it != ranges.end(); ++it)
-        {
-            initial_node->to_select.push_back(it);
-        }
-        initial_node->to_select.sort([](list<Range>::const_iterator a, list<Range>::const_iterator b) { // unit cost from lower to higher
-            return a->cost / a->value < b->cost / b->value;
-        });
+        initial_node->cursor = 0;
         initial_node->bound();
+        initial_node->print("initial node");
     }
 
     auto comp = [](const shared_ptr<Node> &a, const shared_ptr<Node> &b) {
@@ -427,18 +433,14 @@ Bnb::Bnb(const Range &const_universe, const list<Range> &const_ranges, double ta
         auto node = nodes.top();
         nodes.pop();
 
+        sw.pause(); // pause the timer for debug informaiton
+        double t = sw.lap();
         logger << nodes.size() << endl;
-        sw.pause();
-        debug_report.push_back({
-            {"cost", node->cost},
-            {"coverage_ratio", node->value / universe.value},
-            {"selected.size()", node->selected.size()},
-            {"to_select.size()", node->to_select.size()},
-            {"t", sw.lap()},
-            {"number of nodes", nodes.size()},
-            {"cost lower bound", node->cost_lower_bound},
-            {"cost upper bound", optimal_node->cost},
-        });
+        auto desc = node->describe();
+        desc["t"] = t; // to avoid lag
+        desc["number of nodes"] = nodes.size();
+        desc["cost upper bound"] = optimal_node->cost;
+        debug_report.push_back(desc);
         sw.continue_();
 
         if (node->value >= Node::target_value)
@@ -450,7 +452,7 @@ Bnb::Bnb(const Range &const_universe, const list<Range> &const_ranges, double ta
             }
             // else, no lower cost, keeping branching (add more ranges) will be no better, just kill it.
         }
-        else if (node->to_select.size() > 0)
+        else if (node->cursor != Node::ranges.size())
         {
             auto new_nodes = node->branch();
             for (auto new_node : new_nodes)
